b99fd10b6e6b1f8236dac693b300c663aa5c1de3
manjujacob <theresa.jac13@gmail.com>
2015-09-24 22:53:02 +0000

    Use SSL and the Puma webserver in production

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -63,6 +63,6 @@ class UserTest < ActiveSupport::TestCase
   
   test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 5
-    assert @user.valid?
+    assert_not @user.valid?
   end
 end
