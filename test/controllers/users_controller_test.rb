0197d3dae2f0351670b6c67a41748e5f0a781f72
manjujacob <theresa.jac13@gmail.com>
2015-10-10 00:34:55 +0000

    Add user following

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -66,5 +66,16 @@ class UsersControllerTest < ActionController::TestCase
                                             admin: true }
     assert_not @other_user.admin?
   end
+  
+  test "should redirect following when not logged in" do
+    get :following, id: @user
+    assert_redirected_to login_url
+  end
+  
+  test "should redirect followers when not logged in" do
+    get :followers, id: @user
+    assert_redirected_to login_url
+  end
+  
 
 end
